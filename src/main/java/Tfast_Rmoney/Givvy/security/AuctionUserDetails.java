7b2f477e94e503ac71b08fc15bd77f9bf6922c46
Tristan Zook <tristanzook@gmail.com>
2026-05-20 11:04:14 -0500

    Added base security files and folders

---
+++ src/main/java/Tfast_Rmoney/Givvy/security/AuctionUserDetails.java
@@ -0,0 +1,57 @@
+package Tfast_Rmoney.Givvy.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class AuctionUserDetails implements UserDetails {
+	private static final long serialVersionUID = 1L;
+	private String userid;
+	private List<GrantedAuthority> authorities;
+	
+	public AuctionUserDetails(String id) {
+		userid = id;
+		authorities = new ArrayList<GrantedAuthority>();
+		authorities.add(new SimpleGrantedAuthority("USER"));
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return userid;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
